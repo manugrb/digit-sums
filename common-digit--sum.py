8ebd9a1eb37d9f549be36138938c1a9ec5da3ecf
Manuel Groebl <m.groebl@online.de>
2023-02-11 00:42:10 +0100

    Restructure common-digit-sum

--- common-digit--sum.py
+++ common-digit--sum.py
@@ -1,9 +1,15 @@
 from matplotlib import pyplot as plt
 
-maxLimit = int(input("How many numbers should be calculated with? (In dec)"))
-base = int(input("what base do you want to calculate with?"))
+def main():
 
-def getDigitSum(number):
+    maxLimit = int(input("How many numbers should be calculated with? (In dec)"))
+    base = int(input("what base do you want to calculate with?"))
+
+    results = calculateResults(maxLimit, base)
+    printResults(results)
+    plotResults(results)
+
+def getDigitSum(number, base):
 
     digitSum = 0
 
@@ -18,25 +24,39 @@ def getDigitSum(number):
 
     return int(digitSum)
 
-digitSums = []
 
-for n in range(maxLimit):
-    digitSum = getDigitSum(n)
-    
-    # if the array does not have entries for this digitsum yet set the first entry to 1
-    if(digitSum >= len(digitSums)):
-        digitSums.insert(digitSum, 1)
+def calculateResults(maxLimit, base):
 
-    # if the array has entries for this digitsum already, increase it by 1
-    else:
-        digitSums[digitSum] = digitSums[digitSum] + 1
+    digitSums = []
 
-# print out the results
-for i in range(len(digitSums)):
-    print(str(i) + ' -> ' + str(digitSums[i]))
+    for n in range(maxLimit):
+        digitSum = getDigitSum(n, base)
+        
+        # if the array does not have entries for this digitsum yet set the first entry to 1
+        if(digitSum >= len(digitSums)):
+            digitSums.insert(digitSum, 1)
 
-plt.bar(range(len(digitSums)), digitSums)
-plt.title('Digit Sums Distribution')
-plt.xlabel('Digit sums')
-plt.ylabel('Occurences')
-plt.show()
+        # if the array has entries for this digitsum already, increase it by 1
+        else:
+            digitSums[digitSum] = digitSums[digitSum] + 1
+
+    return digitSums
+
+
+def printResults(digitSums):
+
+    # print out the results
+    for i in range(len(digitSums)):
+        print(str(i) + ' -> ' + str(digitSums[i]))
+
+
+def plotResults(digitSums):
+
+    plt.bar(range(len(digitSums)), digitSums)
+    plt.title('Digit Sums Distribution')
+    plt.xlabel('Digit sums')
+    plt.ylabel('Occurences')
+    plt.show()
+
+if(__name__ == "__main__"):
+    main()
