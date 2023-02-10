16af6496a5bb4bf47b4449e6d7f9540d554e700e
Manuel Groebl <m.groebl@online.de>
2023-02-11 00:24:03 +0100

    Add some comments

--- common-digit--sum.py
+++ common-digit--sum.py
@@ -9,12 +9,12 @@ def getDigitSum(number):
 
     while number > 0:
         # get the last digit of number and add it to digitSum
-        lastDigit = number % 10
+        lastDigit = number % base
         digitSum += lastDigit
 
         # remove the last digit from number before repeating the same process again
         number -= lastDigit
-        number = number / 10
+        number = number / base
 
     return int(digitSum)
 
@@ -23,10 +23,14 @@ digitSums = []
 for n in range(maxLimit):
     digitSum = getDigitSum(n)
     
+    # if the array does not have entries for this digitsum yet set the first entry to 1
     if(digitSum >= len(digitSums)):
         digitSums.insert(digitSum, 1)
+
+    # if the array has entries for this digitsum already, increase it by 1
     else:
         digitSums[digitSum] = digitSums[digitSum] + 1
 
+# print out the results
 for i in range(len(digitSums)):
     print(str(i) + ' -> ' + str(digitSums[i]))
