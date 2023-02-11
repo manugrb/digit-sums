7585676ab172a1f6410faaf39eb48ba5ab05d2d2
Manuel Groebl <m.groebl@online.de>
2023-02-11 17:02:44 +0100

    Restructure digit-sums-distribution

--- digit-sums-distribution.py
+++ digit-sums-distribution.py
@@ -1,9 +1,15 @@
 from matplotlib import pyplot as plt
 
-maxLimit = int(input("How many numbers should be calculated with? (In dec)"))
-base = int(input("what base do you want to calculate with?"))
-dampingPower = int(input("what power do you want the damping interval to have?"))
-dampingCoefficient = base ** dampingPower
+
+def main():
+
+    maxLimit = int(input("How many numbers should be calculated with? (In dec)"))
+    base = int(input("what base do you want to calculate with?"))
+    dampingPower = int(input("what power do you want the damping interval to have?"))
+    dampingCoefficient = base ** dampingPower
+    result = calculateResults(maxLimit=maxLimit, base=base, dampingCoefficient=dampingCoefficient)
+    
+    plotResults(result=result)
 
 
 def getDigitSum(number, base):
@@ -21,20 +27,30 @@ def getDigitSum(number, base):
 
     return int(digitSum)
 
-result = []
+def calculateResults(maxLimit, base, dampingCoefficient):
 
-for n in range(int(maxLimit / dampingCoefficient)):
-    totalDigitSum = 0
-    currentNumber = n * dampingCoefficient
+    result = []
 
-    for d in range(dampingCoefficient):
-        totalDigitSum += getDigitSum(currentNumber + d, base)
+    for n in range(int(maxLimit / dampingCoefficient)):
+        totalDigitSum = 0
+        currentNumber = n * dampingCoefficient
 
-    averageDigitSum = totalDigitSum / dampingCoefficient
-    result.append(averageDigitSum)
+        for d in range(dampingCoefficient):
+            totalDigitSum += getDigitSum(currentNumber + d, base)
 
-plt.plot(result)
-plt.title('Digit Sum Distribution')
-plt.xlabel('n of damping intervals')
-plt.ylabel('Digit sums')
-plt.show()
+        averageDigitSum = totalDigitSum / dampingCoefficient
+        result.append(averageDigitSum)
+
+    return result
+
+
+def plotResults(result):
+
+    plt.plot(result)
+    plt.title('Digit Sum Distribution')
+    plt.xlabel('n of damping intervals')
+    plt.ylabel('Digit sums')
+    plt.show()
+
+if(__name__ == "__main__"):
+    main()
